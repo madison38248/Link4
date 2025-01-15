27638eeaeba6cd89e5f9646dceb402fe8180506a
madison38248 <madison38248@gmail.com>
2025-01-15 13:47:44 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.peoplesocks.com/collections/featured-products/products/everyday-merino-tabsocks";
-    const blackPageURL = "https://VXAJXfDwZd.myfunnelish.com/cash-1735686017557538";
+    const whitePageURL = "https://valkayogashop.com/products/yoga-mat-chakra";
+    const blackPageURL = "https://docs.google.com/forms/d/e/1FAIpQLSfwBcqMDMglIuTlk6IgIjMg0W322bwgPBVa-stq5Vs0mZEhGA/viewform?usp=dialog";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
